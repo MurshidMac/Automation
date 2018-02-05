91ebedf45dce773783a15207070776a2b9ca4625
murshidmac <mh.murshidco.uk@outlook.com>
2018-02-05 23:34:47 +0530

    Work Environment is made in eclipse for setting up

---
+++ Automation JAVA/src/AutomationBasic.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class AutomationBasic{
+
+	int AutomationWeb;
+	public static void main(String [] args){
+		System.out.println("This was completly written in Vim. I would Try an Emacs version as well");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
