94288f7d00c85f1e74395cf721b3d1e10752a88b
murshidmac <mh.murshidco.uk@outlook.com>
2018-02-12 22:59:09 +0530

    First Test Case added

--- src/main/java/com/daemon/wordpress/TestDeamon.java
+++ src/main/java/com/daemon/wordpress/TestDeamon.java
@@ -1,9 +1,21 @@
 package com.daemon.wordpress;
 
+import io.ddavison.conductor.Browser;
+import io.ddavison.conductor.Config;
 import io.ddavison.conductor.Locomotive;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+@Config(
+        browser = Browser.CHROME,
+        url="https://daemonxz.wordpress.com/"
+)
 
 public class TestDeamon extends Locomotive {
-
-
-    
+    //first Test
+    @Test
+    public void aboutTest(){
+        // Here goes the id and the invoking
+        click("menu-item-142").click(By.xpath("You can also use different locators "));
+    }
 }
